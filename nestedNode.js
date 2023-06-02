adb465650931233299df0b9742826d1fce7ca97f
ssit <ssit5568@gmail.com>
2023-06-02 11:15:20 -0400

    Can load multiple nested nodes, widgets loading correctly

--- nestedNode.js
+++ nestedNode.js
@@ -1,189 +1,32 @@
 import {app} from "../../scripts/app.js";
-import {ComfyWidgets} from "../../scripts/widgets.js";
-import {ext} from "./nodeMenu.js";
 
 // Node that allows you to convert a set of nodes into a single node
 export const nestedNodeType = "NestedNode";
 export const nestedNodeTitle = "Nested Node";
 
-export class NestedNode {
+export function serializeWorkflow(workflow) {
+    let nodes = [];
+    for (const id in workflow) {
+        const node = workflow[id];
+        nodes.push(LiteGraph.cloneObject(node.serialize()));
+    }
+    return nodes;
+}
 
-    widgetsToSerializedWidgets = {};
+export class NestedNode {
 
     // Nest the workflow within this node
     nestWorkflow(workflow) {
-        // Add this node to the graph
-        app.graph.add(this);
-
         // Node setup
-        this.properties.serializedWorkflow = this.serializeWorkflow(workflow);
-        console.log("Workflow being nested:", workflow);
-        this.inheritInputs(workflow);
-        this.inheritOutputs(workflow);
-        this.inheritWidgets();
+        this.properties.serializedWorkflow = serializeWorkflow(workflow);
         this.placeNestedNode(workflow);
         this.resizeNestedNode(workflow);
         this.removeNestedNodes(workflow);
-        console.log("The resulting nested node:", this);
-
+        console.log(this.serialize);
     }
 
     refreshNest() {
         this.inheritWidgets();
-    }
-
-    // How the workflow is stored
-    serializeWorkflow(workflow) {
-        let nodes = [];
-        for (const id in workflow) {
-            const node = workflow[id];
-            nodes.push(node.serialize());
-        }
-        return nodes;
-    }
-
-    // Add inputs to the node
-    inheritInputs(workflow) {
-        this.inputs = [];
-        let nested_slot = -1;
-
-        // Iterate over each node in the workflow
-        for (const id in workflow) {
-            const node = workflow[id];
-            // Iterate over each input in the node
-            for (const slot in node.inputs) {
-                // Check if the input is connected by another node in the workflow
-                const inputNode = node.getInputNode(slot);
-                if (Object.values(workflow).includes(inputNode)) {
-                    // If so, skip this input
-                    continue;
-                }
-                // Otherwise, add the input to the node
-                const inputInfo = node.getInputInfo(slot);
-                this.addInput(inputInfo.name, inputInfo.type);
-                nested_slot++;
-                // Add the connection to the input if it exists
-                const link = node.getInputLink(slot);
-                if (link) {
-                    inputNode.connect(link.origin_slot, this, nested_slot);
-                }
-            }
-        }
-    }
-
-    // Add outputs to the node
-    inheritOutputs(workflow) {
-        this.outputs = [];
-        let nested_slot = -1;
-
-        // Iterate over each node in the workflow
-        for (const id in workflow) {
-            const node = workflow[id];
-            // Iterate over each output in the node
-            for (const slot in node.outputs) {
-                // Check if the output only goes to other nodes in the workflow
-                const outputNodes = node.getOutputNodes(slot);
-                if (!outputNodes) {
-                    continue;
-                }
-                let onlyWorkflow = true;
-                for (const output of outputNodes) {
-                    if (!Object.values(workflow).includes(output)) {
-                        onlyWorkflow = false;
-                        break;
-                    }
-                }
-                if (onlyWorkflow) {
-                    // If so, skip this output
-                    continue;
-                }
-                // Otherwise, add the output to the node
-                const outputInfo = node.getOutputInfo(slot);
-                this.addOutput(outputInfo.name, outputInfo.type);
-                nested_slot++;
-                // Adding connections
-                const linkIDs = outputInfo.links;
-                for (const id of linkIDs) {
-                    const link = app.graph.links[id];
-                    // Add the connection if the target node is not in the workflow
-                    const targetNode = app.graph.getNodeById(link.target_id);
-                    if (!Object.values(workflow).includes(targetNode)) {
-                        this.connect(nested_slot, targetNode, link.target_slot);
-                    }
-                }
-            }
-        }
-    }
-
-    // inheritWidgets() {
-    //     const workflow = this.properties.serializedWorkflow;
-    //     this.widgets = [];
-    //     this.widgets_values = [];
-    //     const widgets = ComfyWidgets;
-    //     for (const serialized of workflow) {
-    //         const type = serialized.type;
-    //         const def = ext.defs[type];
-    //         const required = def.input.required;
-    //         const optional = def.input.optional;
-    //         let inputs = required;
-    //         if (optional) {
-    //             inputs = Object.assign({}, required, optional);
-    //         }
-    //         for (const inputName in inputs) {
-    //             const inputData = inputs[inputName];
-    //             const type = inputData[0];
-    //
-    //             if(inputData[1]?.forceInput) {
-    //                 this.addInput(inputName, type);
-    //             } else {
-    //                 let isWidget = false;
-    //                 if (Array.isArray(type)) {
-    //                     // Enums
-    //                     widgets.COMBO(this, inputName, inputData, app);
-    //                     isWidget = true;
-    //                 } else if (`${type}:${inputName}` in widgets) {
-    //                     // TODO: Support custom widgets
-    //                     isWidget = true;
-    //                 } else if (type in widgets) {
-    //                     // Standard type widgets
-    //                     widgets[type](this, inputName, inputData, app);
-    //                     isWidget = true;
-    //                 }
-    //                 // TODO: Might inherit inputs here instead of a different method
-    //                 // else {
-    //                 //     // Node connection inputs
-    //                 //     this.addInput(inputName, type);
-    //                 // }
-    //             }
-    //         }
-    //         // Set the widget to the value stored in the serialized workflow
-    //         this.widgets_values = this.widgets_values.concat(serialized.widgets_values);
-    //     }
-    //     this.configure({title: this.type, widgets_values: this.widgets_values});
-    //     // this.configure(this);
-    //     for (const widget of this.widgets) {
-    //         if (widget.inputEl) {
-    //             widget.inputEl.addEventListener("change", () => {
-    //                 this.refreshWidgets();
-    //             });
-    //         }
-    //     }
-    // }
-
-    inheritWidgets() {
-        const workflow = this.properties.serializedWorkflow;
-        this.widgets = [];
-        this.widgets_values = [];
-
-        for (const node_s of workflow) {
-            const type = node_s.type;
-            const def = ext.defs[type];
-            const node = LiteGraph.createNode(type);
-            node.configure(def);
-            this.widgets_values = this.widgets_values.concat(node_s.widgets_values);
-            this.widgets = this.widgets.concat(node.widgets);
-        }
-        this.configure({title: this.type, widgets_values: this.widgets_values});
     }
 
     // Remove the nodes that are being nested
@@ -230,6 +73,8 @@ export class NestedNode {
         if (name === "serializedWorkflow") {
             console.log("serializedWorkflow changed", value);
         }
+
+        console.log(this.serialize);
     }
 
     refreshWidgets() {
@@ -246,4 +91,17 @@ export class NestedNode {
     onWidgetChanged(name, value, old_value, widget) {
         this.refreshWidgets();
     }
+
+    // onNodeCreated() {
+    //     // Replace the unique nested node type with the generic nested node type during serialization
+    //     const serialize = this.serialize;
+    //     this.serialize = function() {
+    //         const uniqueType = this.type;
+    //         this.type = nestedNodeType;
+    //         const serialized = serialize.call(this);
+    //         this.type = uniqueType;
+    //         return serialized;
+    //     }
+    // }
+
 }
--- nodeMenu.js
+++ nodeMenu.js
@@ -1,53 +1,117 @@
 import {app} from "../../scripts/app.js";
-import {api} from "../../scripts/api.js";
-import {NestedNode, nestedNodeTitle, nestedNodeType} from "./nestedNode.js";
+import {NestedNode, nestedNodeTitle, nestedNodeType, serializeWorkflow} from "./nestedNode.js";
 
 export const ext = {
-    name: "SS.NestedNodeBuilder",
-    defs: {},
+    name: "SS.NestedNodeBuilder", defs: {}, nestedDef: {}, nestedNodeDefs: {}, nestedNodeId: 0,
 
+    /**
+     * Called before the app registers nodes from definitions.
+     * Used to add nested node definitions.
+     * @param defs The node definitions.
+     * @param app The app.
+     * @returns {Promise<void>}
+     */
     async addCustomNodeDefs(defs, app) {
-        console.log(defs);
-        defs[nestedNodeType] = {
-            category: "Not For Use",
-            type: nestedNodeType,
-            description: "",
-            display_name: "MUST_CREATE_NESTED_NODE_FROM_SELECTION",
-            input: {required: {}},
-            name: nestedNodeType,
-            output: {},
-            output_is_list: [],
-            output_node: false,
-        };
-        console.log(api);
-        ext.defs = defs;
+        // Save definitions for reference
+        this.defs = defs;
+        // Add nested node definitions if they exist
+        Object.assign(defs, this.nestedNodeDefs);
+        // Default nested node definition
+        if (Object.keys(this.nestedNodeDefs).length === 0) {
+            defs[nestedNodeType] = {
+                category: "NOT_FOR_USE",
+                display_name: nestedNodeTitle,
+                name: nestedNodeType,
+                input: {required: {}},
+                output: [],
+                output_name: [],
+            };
+        }
+        console.log("[SS] Added nested node definitions:", defs);
     },
 
+    /**
+     * Called after inputs, outputs, widgets, menus are added to the node given the node definition.
+     * Used to add methods to nested nodes.
+     * @param nodeType The ComfyNode object to be registered with LiteGraph.
+     * @param nodeData The node definition.
+     * @param app The app.
+     * @returns {Promise<void>}
+     */
     async beforeRegisterNodeDef(nodeType, nodeData, app) {
-        if (nodeData.name !== nestedNodeType) {
+        // Return if the node is not a nested node
+        if (Object.keys(this.nestedNodeDefs).length === 0) {
             return;
         }
+        let isNestedNode = false;
+        for (const defName in this.nestedNodeDefs) {
+            if (defName === nodeData.name) {
+                isNestedNode = true;
+                break;
+            }
+        }
+        if (!isNestedNode) {
+            return;
+        }
+        console.log("[SS] Adding nested node methods:", nodeType.prototype);
+
+
+        // Add Nested Node methods to the node ComfyNode
         const nestedNodePrototype = NestedNode.prototype;
-        // Add the methods from the prototype to the node
         for (const key of Object.getOwnPropertyNames(nestedNodePrototype)) {
             nodeType.prototype[key] = nestedNodePrototype[key];
         }
         nodeType.prototype.isVirtualNode = true;
+        console.log("[SS] Added nested node methods:", nodeType.prototype);
     },
 
+    /**
+     * Called when loading a graph from a JSON file or pasted into the app.
+     * @param node The node that was loaded.
+     * @param app The app.
+     */
     loadedGraphNode(node, app) {
-        if (node.type !== nestedNodeType) {
+        // Return if the node is not a nested node
+        if (!node.properties.serializedWorkflow) {
             return;
         }
-        node.refreshNest();
+
+        // Use the serialized workflow to create a nested node definition
+        const nestedDef = this.createNestedDef(node.properties.serializedWorkflow);
+
+        //
+        // If the definition already exists, then the node will be loaded with the existing definition
+        //
+        for (const defName in this.nestedNodeDefs) {
+            const def = this.nestedNodeDefs[defName];
+            if (isSerializedWorkflowsEqual(def.description, nestedDef.description)) {
+                return;
+            }
+        }
+
+        //
+        // Otherwise, add the definition and reload the graph
+        //
+        // Add the def
+        this.nestedNodeDefs[nestedDef.name] = nestedDef;
+        // Reload the graph
+        app.registerNodes().then(() => {
+            // Reload the graph data
+            app.loadGraphData(app.graph.serialize());
+        }, (error) => {
+            console.log("Error registering nodes:", error);
+        });
     },
 
+    /**
+     * Called when a node is created. Used to add menu options to nodes.
+     * @param node The node that was created.
+     * @param app The app.
+     */
     nodeCreated(node, app) {
-        //
-        // Add menu option for nesting nodes for ComfyNodes
-        //
         // Save the original options
         const getBaseMenuOptions = node.getExtraMenuOptions;
+        // Add new options
         node.getExtraMenuOptions = function (_, options) {
             // Call the original function for the default menu options
             getBaseMenuOptions.call(this, _, options);
@@ -64,15 +128,71 @@ export const ext = {
         };
     },
 
+    createNestedDef(serializedWorkflow) {
+        const uniqueId = this.nestedNodeId++;
+        let nestedDef = {
+            name: nestedNodeType + uniqueId,
+            display_name: nestedNodeTitle,
+            category: "Nested Nodes",
+            description: serializedWorkflow,
+            input: {},
+            output: [],
+            output_is_list: [],
+            output_name: [],
+            nested_node_id: uniqueId,
+        };
+        for (const id in serializedWorkflow) {
+            const node = serializedWorkflow[id];
+            const nodeDef = this.defs[node.type];
+            // Concatenate inputs
+            for (const inputType in nodeDef.input) {
+                nestedDef.input[inputType] = Object.assign({}, nestedDef.input[inputType], nodeDef.input[inputType]);
+            }
+            // Concatenate outputs
+            nestedDef.output = nestedDef.output.concat(nodeDef.output);
+            nestedDef.output_name = nestedDef.output_name.concat(nodeDef.output_name);
+            nestedDef.output_is_list = nestedDef.output_is_list.concat(nodeDef.output_is_list);
+        }
+        return nestedDef;
+    },
+
     nestSelectedNodes() {
         // Use the selected nodes for the nested node
         const selectedNodes = app.canvas.selected_nodes;
 
-        // Create the nested node
-        const nestedNode = LiteGraph.createNode(nestedNodeType);
-        nestedNode.title = nestedNodeTitle;
-        nestedNode.nestWorkflow(selectedNodes);
+        // Add a custom definition for the nested node
+        const nestedDef = this.createNestedDef(serializeWorkflow(selectedNodes));
+
+        // Add the def, this will be added to defs in addCustomNodeDefs
+        this.nestedNodeDefs[nestedDef.name] = nestedDef;
+
+        // Register nodes again to add the nested node definition
+        app.registerNodes().then(() => {
+            // Create the nested node
+            const nestedNode = LiteGraph.createNode(nestedDef.name);
+            app.graph.add(nestedNode);
+            nestedNode.nestWorkflow(selectedNodes);
+        }, (error) => {
+            console.log("Error registering nodes:", error);
+        });
     },
 };
 
 app.registerExtension(ext);
+
+
+function isSerializedWorkflowsEqual(a, b) {
+    // Test if two serialized workflows are equal
+    if (a.length !== b.length) {
+        return false;
+    }
+    for (const i in a) {
+        const nodeA = a[i];
+        const nodeB = b[i];
+        // Types should be equal
+        if (nodeA.type !== nodeB.type) {
+            return false;
+        }
+    }
+    return true;
+}
