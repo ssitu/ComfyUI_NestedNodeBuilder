a00e26ccaa78ca7ef648a34c160f879e2fec61ab
ssit <ssit5568@gmail.com>
2023-07-31 14:10:00 -0400

    Locate base directory in a better way

--- directory_paths.py
+++ directory_paths.py
@@ -1,8 +1,10 @@
 import os
+import folder_paths
 
+comfy_path = os.path.dirname(folder_paths.__file__)
 ext_path = os.path.dirname(os.path.realpath(__file__))
 repo_name = os.path.basename(ext_path)
 default_nested_nodes_path = os.path.join(ext_path, "nested_nodes")
-js_extensions_path = os.path.join(ext_path, os.pardir, os.pardir, "web", "extensions")
+js_extensions_path = os.path.join(comfy_path, "web", "extensions")
 config_path = os.path.join(ext_path, "config.yaml")
 js_extensions_repo_path = os.path.join(js_extensions_path, repo_name)
